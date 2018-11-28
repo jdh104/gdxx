c2e0110f7ca8ca2eaffdb7a2cfec70db6afa1db2
Jonah <jdh104@latech.edu>
2018-11-28 15:40:55 -0600

    Add vector scaling operations

--- core/src/com/jonahhaney/gdxx/box2d/Physics.java
+++ core/src/com/jonahhaney/gdxx/box2d/Physics.java
@@ -4,6 +4,8 @@ import java.awt.AWTError;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * TODO Document
  * 
@@ -22,11 +24,12 @@ public class Physics {
 
     // Calculate actual-size ppm
     static {
-        float default_ppm = (float)STANDARD_PPI * INCHES_PER_METER;
+        float default_ppm = (float) STANDARD_PPI * INCHES_PER_METER;
         try {
             default_ppm = Toolkit.getDefaultToolkit().getScreenResolution() * INCHES_PER_METER;
         } catch (AWTError | HeadlessException ex) {
-            System.err.println("Could not get screen resolution, using industry standard of " + STANDARD_PPI + " ppi instead.");
+            System.err.println(
+                    "Could not get screen resolution, using industry standard of " + STANDARD_PPI + " ppi instead.");
         } finally {
             DEFAULT_PPM = default_ppm;
         }
@@ -46,10 +49,28 @@ public class Physics {
 
     /**
      * 
+     * @param mVector
+     * @return
+     */
+    public static Vector2 metersToPixels(Vector2 mVector) {
+        return mVector.scl(Physics.pixelsPerMeter);
+    }
+
+    /**
+     * 
      * @param pixels
      * @return
      */
     public static float pixelsToMeters(float pixels) {
         return pixels / Physics.pixelsPerMeter;
+    }
+
+    /**
+     * 
+     * @param pVector
+     * @return
+     */
+    public static Vector2 pixelsToMeters(Vector2 pVector) {
+        return pVector.scl(1 / Physics.pixelsPerMeter);
     }
 }
