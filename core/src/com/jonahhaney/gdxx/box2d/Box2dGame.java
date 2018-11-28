ed36a473065d63ffa24a45491142095804ddd7c1
Jonah <jdh104@latech.edu>
2018-11-28 15:13:23 -0600

    Clean up B2dGame class

--- core/src/com/jonahhaney/gdxx/box2d/Box2dGame.java
+++ core/src/com/jonahhaney/gdxx/box2d/Box2dGame.java
@@ -1,35 +1,20 @@
 package com.jonahhaney.gdxx.box2d;
 
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.jonahhaney.gdxx.LibGdxGame;
 
 /**
- * TODO Document
+ * TODO Document TODO Decide if this class is necessary
  * 
  * @author Jonah Haney
  */
 public abstract class Box2dGame extends LibGdxGame {
 
-    private Box2DDebugRenderer b2dr;
-    private OrthographicCamera b2drCam;
-    private boolean debugBox2d;
-
+    /**
+     * 
+     * @param debugBox2d
+     */
     public Box2dGame(boolean debugBox2d) {
         super();
-        /*this.debugBox2d = debugBox2d;
-
-        if (debugBox2d) {
-            b2dr = new Box2DDebugRenderer();
-            b2drCam = new OrthographicCamera();
-            b2drCam.setToOrtho(false, Physics.pixelsToMeters(super.settings.getWidth()),
-                    Physics.pixelsToMeters(super.settings.getHeight()));
-        }*/
-    }
-    
-    @Override
-    public void render() {
-        super.render();
     }
 
     @Override
