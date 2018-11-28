590000787c917031664d7cbc96e399c9859b33b5
Jonah <jdh104@latech.edu>
2018-11-28 15:43:49 -0600

    Allow debugging to be passed through Box2dGame

--- core/src/com/jonahhaney/gdxx/box2d/Box2dGame.java
+++ core/src/com/jonahhaney/gdxx/box2d/Box2dGame.java
@@ -9,14 +9,15 @@ import com.jonahhaney.gdxx.LibGdxGame;
  */
 public abstract class Box2dGame extends LibGdxGame {
 
-    protected Box2dSettings settings = new Box2dSettings(false);
+    protected Box2dSettings settings;
     
     /**
      * 
      * @param debugBox2d
      */
-    public Box2dGame() {
+    public Box2dGame(boolean debugBox2d) {
         super();
+        this.settings = new Box2dSettings(debugBox2d);
     }
 
     @Override
